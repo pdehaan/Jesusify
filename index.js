d62b983234059b7173f2770fea610a8963a9d9c7
azuelsdorf16 <azuelsdorf16@bitbucket.org>
2016-08-27 12:48:27 -0400

    Finished Jesusify minimal implementation.

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 exports.jimp = require("jimp");
 
-exports.quotes = [
-	"I have told you these things, so that in me you mave have peace. In this world you will have trouble. But take heart! I have overcome the world. --John 16:33 (NIV)",
-	"If the world hates you, understand that it hated Me first. --John 15:18 (NIV)",
-	"Ask and it will be given to you; seek and you will find; knock and the door will be opened to you. --Matthew 7:7 (NIV)",
-	"You see, at just the right time, when we were still powerless, Christ died for the ungodly. --Romans 5:6 (NIV)",
-	"The God of peace will soon crush Satan under your feet. The grace of our Lord Jesus Christ be with you. --Romans 16:20 (NIV)",
-	"The grace of our Lord Jesus Christ by with you all. Amen. --Revelation 22:21 (KJV)"
-];
-
 exports.printImage = function (destinationImage, inFileName, outFileName,
 								callbackFunction, origWidth, origHeight) {
 	exports.jimp.read(inFileName, function (error1, image) {
@@ -17,14 +8,16 @@ exports.printImage = function (destinationImage, inFileName, outFileName,
 			return;
 		}
 
-		image.resize(destinationImage.bitmap.width >> 1, destinationImage.bitmap.height >> 1);
+		image.resize(destinationImage.bitmap.width >> 1,
+					destinationImage.bitmap.height >> 1);
 
 		destinationImage.composite(image,
 			destinationImage.bitmap.width - image.bitmap.width,
 			destinationImage.bitmap.height - image.bitmap.height
 		);
 
-		callbackFunction(null, outFileName, destinationImage, origWidth, origHeight);
+		callbackFunction(null, outFileName, destinationImage, origWidth,
+						origHeight);
 	});
 }
 
@@ -36,14 +29,13 @@ exports.printMessage = function (error, outFileName, destinationImage,
 	}
 
 	exports.jimp.loadFont(
-'fonts/open-sans-64-red/open-sans-64-red.fnt').then(function (font) {
-		var quotes = exports.quotes;
+	'fonts/open-sans-64-red/open-sans-64-red.fnt').then(function (font) {
 		var width = destinationImage.bitmap.width;
 		var height = destinationImage.bitmap.height;
-		var index = Math.floor(Math.random() * quotes.length) % quotes.length;
-		destinationImage.print(font, width >> 3, height / 3,
-			quotes[index] + "", width - width >> 2);
-		destinationImage.resize(origWidth, origHeight);
+		var message = "Jesus loves you!";
+
+		destinationImage.print(font, width >> 3, height >> 3, message,
+								width - width >> 2);
 		destinationImage.write(outFileName);
 	}).catch(function (error2) {
 		console.error("Received error: \"" + error2 + "\"");
@@ -54,28 +46,34 @@ exports.jesusify = function (fileName, outputName) {
 	exports.jimp.read(fileName, function (error1, image) {
 		if (error1) {
 			console.error("Received error: \"" + error1 + "\"");
-			process.exit(-1);
+			return;
 		}
 
-		//Output file name must be a string and must have a .png extension.
+		//Output file name must be a string and must have a .jpeg extension.
 		if (outputName === undefined || outputName === null) {
-			outputName = "out.png";
+			outputName = "out.jpeg";
 		}
 		else if (typeof outputName !== "string") {
-			outputName = "out.png";
+			outputName = "out.jpeg";
 		}
-		else if (!outputName.endsWith(".png")) {
-			outputName += ".png";
+		else if (!outputName.endsWith(".jpeg") && !outputName.endsWith(".jpg")) {
+			outputName += ".jpeg";
 		}
 
 		var origWidth = image.bitmap.width;
 		var origHeight = image.bitmap.width;
 
-		image.resize(Math.max(image.bitmap.width, 512),
-					Math.max(image.bitmap.height, 512));
-		exports.printImage(image, "images/jesus.png", "out.png",
+		exports.printImage(image, "images/jesus.png", outputName,
 							exports.printMessage, origWidth, origHeight);
 	});
 };
 
-exports.jesusify("images/person.jpeg");
+console.log("Starting Clinton");
+exports.jesusify("images/clinton.jpeg", "clinton.jpeg");
+console.log("Finished Clinton");
+console.log("Starting Trump");
+exports.jesusify("images/trump.jpeg", "trump.jpeg");
+console.log("Finished Trump");
+console.log("Starting picture");
+exports.jesusify("images/picture.jpeg", "picture.jpeg");
+console.log("Finished picture");
