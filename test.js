5ea24f136f954adfb9fc546cc756e3c0b272f132
azuelsdorf16 <azuelsdorf16@bitbucket.org>
2016-08-30 21:13:21 -0400

    Fixed problem where test would not run if fed to node from a parent dir.

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 var jesusify = require('./index.js');
+var path = require('path');
 
 console.log("Converting first picture.");
-jesusify.jesusify("images/martinLuther.png", "martinLuther.png",
-	function (err, response) {
+jesusify.jesusify(path.join(__dirname, "images/martinLuther.png"),
+				path.join(__dirname, "martinLuther.png"),
+	function (err, image) {
 		if (err) {
 			console.error("Received error: \"" + err + "\"");
 			return;
 		}
 
 		console.log("Writing picture");
-		response.write("martinLuther2.png");
+		image.write(path.join(__dirname, "martinLuther2.png"));
 		console.log("Done writing pictures. martinLuther.png" +
 					" and martinLuther2.png should have the" +
 					" same sha1sum and look the same.");
